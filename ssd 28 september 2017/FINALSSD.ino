11f6644945cb8d02fd5f13fc069d702f53c865d1
pmapfeka2014 <31663476+pmapfeka2014@users.noreply.github.com>
2017-09-28 00:02:50 -0400

    Add files via upload

---
+++ ssd 28 september 2017/FINALSSD.ino
@@ -0,0 +1,35 @@
+int outputenable=3;
+int data=2;
+int storeclk=4;
+int shiftclk=5;
+
+
+void setup() {
+  // put your setup code here, to run once:
+int x;
+for (x=0;x<7;x++)
+{
+  pinMode(x,OUTPUT);
+  
+}
+
+digitalWrite(storeclk,LOW);
+digitalWrite(outputenable,LOW);//ACTIVE LOW
+int v;
+for(v=0;v<2;v++)
+{
+shiftOut(data, shiftclk, MSBFIRST,54); 
+shiftOut(data, shiftclk, MSBFIRST,37); 
+
+}
+
+digitalWrite(storeclk,HIGH);
+digitalWrite(storeclk,LOW);
+ 
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
