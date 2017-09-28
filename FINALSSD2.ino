2f6cb42768aa7ca8f57b97637b370e8cb0c047e2
pmapfeka2014 <31663476+pmapfeka2014@users.noreply.github.com>
2017-09-28 00:42:45 -0400

    Add files via upload
    Added decoder for numbers 1 to 9. in the while loop, put it to 1 second delay. still missing decimal to binary functionality

---
+++ FINALSSD2.ino
@@ -0,0 +1,91 @@
+int outputenable=3;
+int data=2;
+int storeclk=4;
+int shiftclk=5;
+int SSD_Decode(int number);
+void displaynum(int x);
+int v;
+
+
+void setup() {
+  // put your setup code here, to run once:
+int x;
+for (x=0;x<7;x++)
+{
+  pinMode(x,OUTPUT);
+  
+}
+
+digitalWrite(storeclk,LOW);
+digitalWrite(outputenable,LOW);//ACTIVE LOW
+
+
+
+}
+
+void displaynum(int x)
+{
+  int y=SSD_Decode(x);
+  shiftOut(data, shiftclk, MSBFIRST,y);
+  digitalWrite(storeclk,HIGH);
+digitalWrite(storeclk,LOW);
+ 
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+for(v=0;v<10;v++)
+{
+
+displaynum(v);
+delay(1000);
+
+}
+
+}
+
+int SSD_Decode(int number)//decoder
+{
+  int x=0;
+  switch (number)
+  {
+     case 0:
+    x= 132;
+    break;
+    case 1:
+    x=246;
+    break;
+    case 2:
+    x=76;
+     break;
+
+      case 3:
+    x=100;
+     break;
+      case 4:
+  x=54;
+     break;
+      case 5:
+   x=37;
+     break;
+      case 6:
+   x=5;
+     break;
+      case 7:
+    x=244;
+     break;
+      case 8:
+x=4;
+     break;
+      case 9:
+     x=36;
+     break;
+    
+
+
+     
+     
+}
+return x;
+}
